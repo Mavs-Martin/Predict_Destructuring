47b22782c3f7445cd75735b9c674904dc2ca4b11
Mavs-Martin <124220815+Mavs-Martin@users.noreply.github.com>
2023-05-01 19:03:18 -0700

    first commit

---
+++ P_D.js
@@ -0,0 +1,49 @@
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output telsa, mercedes
+// console.log(randomCar)
+// console.log(otherRandomCar)
+
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output elon then full info
+// console.log(name);
+// console.log(otherName);
+// //gave an error
+
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// // }
+// // const password = '12345';
+// // const { password: hashedPassword } = person;  
+// // //Predict the output reads password and undefined
+// // console.log(password);
+// // console.log(hashedPassword);
+
+// // const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+// // const [,first] = numbers;
+// // const [,,,second] = numbers;
+// // const [,,,,,,,,third] = numbers;
+// // //Predict the output false true
+// // console.log(first == second);
+// // console.log(first == third);
+
+// const lastTest = {
+//     key: 'value',
+//     secondKey: [1, 5, 1, 8, 3, 3]
+// }
+// const { key } = lastTest;
+// const { secondKey } = lastTest;
+// const [ ,willThisWork] = secondKey;
+// //Predict the output value , [1,5,1,8,3,3], 1, 5
+// console.log(key);
+// console.log(secondKey);
+// console.log(secondKey[0]);
+// console.log(willThisWork);
